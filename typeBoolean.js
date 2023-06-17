26bb4905375e0f0a36a740c2cde333a1c59def7c
Lucas Firpo <lucasfreiref@icloud.com>
2023-06-17 10:17:05 -0300

    primeira aula

---
+++ typeBoolean.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = "Olá, Mundo!";
+const senha = "senhaDificil";
+const StringDeNumeros = "3515";
+
+const citacao = "Meu nome é ";
+const meuNome = "Lucas";
+
+//concatenação "+"
+
+console.log(citacao + meuNome);
---
+++ typeNumber.js
@@ -0,0 +1,6 @@
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacao = segundoNumero * primeiroNumero;
+
+console.log(operacao);
