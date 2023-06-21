4a47ab64b2f9274da7c065e49fefa2280c2b16d4
Lucas Firpo <lucasfreiref@icloud.com>
2023-06-20 21:27:34 -0300

    templatejs

---
+++ templateTernario.js
@@ -0,0 +1,9 @@
+const nome = "Lucas";
+const idade = 2023 - 2000;
+const cidadeNascimento = "Porto Alegre";
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+console.log(apresentacao)
+console.log(idade >= 18 ? "Sou maior de 18" : "Sou menor de idade");
+
+
